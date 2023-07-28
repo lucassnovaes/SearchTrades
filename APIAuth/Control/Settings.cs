c7dac415bac0d8f462ef40234f9f4c71b80553d7
lucassnovaes <54210161+lucassnovaes@users.noreply.github.com>
2023-07-27 22:18:10 -0300

    Inclusão JWT

---
+++ APIAuth/Control/Settings.cs
@@ -0,0 +1,18 @@
+﻿namespace APIAuth.Control
+{
+    public static class Settings
+    {
+        private static IConfigurationBuilder GetConfigurationBuilder()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+        }
+        public static string Secret()
+        {
+            var builder = GetConfigurationBuilder();
+            var config = builder.Build();
+            return config[$"Security:Token"].ToString();
+        }
+    }
+}
