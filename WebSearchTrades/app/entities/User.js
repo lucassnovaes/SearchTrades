03b67ecfb937b2812883ee9f08aef9e3726cdde5
Matheus Vicente <m7vicente@users.noreply.github.com>
2023-07-29 12:33:31 -0300

    feature: Added next app frontend

---
+++ WebSearchTrades/app/entities/User.js
@@ -0,0 +1,26 @@
+class User {
+  constructor(username, password, role) {
+    this.username = username;
+    this._password = password;
+    this.role = role;
+  }
+
+  fromObject({ username, password, role }) {
+    return new this(username, password, role);
+  }
+
+  setToken({ Expiration, AccessToken }) {
+    this._expiration = Expiration;
+    this._accessToken = AccessToken;
+  }
+
+  getAccessToken() {
+    return this._accessToken;
+  }
+
+  toString() {
+    return JSON.stringify(this);
+  }
+}
+
+export default User;
